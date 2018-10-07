4ddcae199bad5b0c23a5618b9e6184ad853e80ee
Mogeley <cmd4life2@yahoo.com>
2018-10-07 14:29:15 -0700

    finished build script.

--- build.ps1
+++ build.ps1
@@ -1,6 +1,22 @@
-$exclude = @("build.ps1","*.zip")
+$info = (Get-Content info.json) -join "`n" | ConvertFrom-Json
+$version = $info.version
+
+$filename = "AutoFactoryBuilder$version"
+
+$exclude = @("build.ps1",".gitignore","*.zip","AutoFactoryBuilder*")
+
+Write-Host $filename
+
+New-Item -Path ".\" -Name $filename -ItemType "directory" -Force
+
 Get-ChildItem -Path $from -Recurse -Exclude $exclude |
     ForEach-Object { 
         Write-Host $_
-        Compress-Archive -LiteralPath $_ -CompressionLevel Optimal -DestinationPath test.zip -Update
+        Copy-Item $_ -Destination ".\$filename" -force
     }
+
+Compress-Archive -Path .\$filename -CompressionLevel Optimal -DestinationPath "$filename.zip" -Update
+
+Write-Host "Zip $filename.zip written"
+
+Remove-Item -Path .\$filename -Recurse -Force
